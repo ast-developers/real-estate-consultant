a7a92751c8b182e9af261b8ce7c1f57f9a21e771
Mehul Dobariya <developers@arsenaltech.com>
2018-05-07 11:32:57 +0530

    Changed page content (#7)

--- resources/views/cms/faq.blade.php
+++ resources/views/cms/faq.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>FAQ</h1><br>
+    <h1>FAQ's</h1><br>
     <p>We attempt to make our reports and systems as easy to use as possible. If you have questions see the below Frequently Asked Questions of our reports and process. We update them regularly to help to keep our customers informed. If you still have questions or suggestions please contact us through our contact page and someone will respond to you as soon as possible.</p>
     <img class="img-responsive mb-3 hero-banner" src="{!! url('public/images/faq.jpg') !!}">
     <div id="accordion">
@@ -16,7 +16,7 @@
 
             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="card-body">
-                    Since the supreme court case of Village of Euclid v. Amber Realty Co. in 1926 zoning has become more and more prevalent in the United States. In that case the Village of Euclid had 6 zoning classes that were affirmed by the supreme court. The court cited that zoning provided for the “general health and welfare” of the public. The intent of the ordinance was to separate noxious or undesired uses from neighboring residential properties that could change the character of the neighborhood. Now, for instance, the city of Atlanta GA has over 60 zoning classes with restrictions including land use, parking, building heights, build setbacks, tree canopy coverage, building materials, lighting. These 60 zoning classes can also be affected by overlay districts and historic districts that can add complexity to each property exponentially.
+                    Since the supreme court case of Village of Euclid v. Amber Realty Co. in 1926, zoning has become more and more prevalent in the United States. In that case the Village of Euclid had 6 zoning classes that were affirmed by the supreme court. The court cited that zoning provided for the “general health and welfare” of the public. The intent of the ordinance was to separate noxious or undesired uses from neighboring residential properties that could change the character of the neighborhood. The city of Atlanta GA has over 60 zoning classes with restrictions including land use, parking, building heights, build setbacks, tree canopy coverage, building materials, lighting. These 60 zoning classes can also be affected by overlay districts and historic districts that can add complexity to each property exponentially.
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                 <div class="card-body">
-                    Different groups have different reasons for needing a zoning report. Of all the groups each realize that a zoning report can save significant amounts of money and mitigate risk. Some groups need a report to verify the zoning of a property and verify that their proposed use for the property is allowed. Some groups need a report to verify that the current use of the property will be allowed when the property changes hands or is damaged due to fire, flood or accidental destruction. Some groups use our reports as an augmentation to their own research this allows professionals from other sectors like surveyors, lenders and zoning attorneys to minimize their risk of providing information that may not be their core competency. Reliant specializes in Zoning research and can do it efficiently and completely. Zoning research is all we do.
+                    A zoning report is a tool that is used to make decisions regarding risk and the feasibility of a property before significant investments are made. Some groups need a report to verify the zoning of a property and verify that their proposed use for the property is allowed. Some groups need a report to verify that the current use of the property will be allowed when the property changes hands or is damaged due to fire, flood or accidental destruction. Some groups use our reports as an augmentation to their own research to minimize their risk of providing information that may not be their core competency. Reliant specializes in Zoning research and can do it efficiently and completely. Zoning research is all we do.
                 </div>
             </div>
         </div>
@@ -58,8 +58,8 @@
             </div>
             <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordion">
                 <div class="card-body">
-                    As land developers and former municipal planners, Reliant looks at zoning differently and can provide insights on what are potentially risky issues that may cost you money and what issues are will not amount to problems and can be ignored. Often municipalities intend for their zoning ordinances to be very clear however many have vague clauses that can leave its meaning up to interpretation<br><br>
-                    Reviewers carry bachelor’s degrees in land use and real estate with a wide range of experiences and educations across all facets of land use and development.
+                    As land developers and former municipal planners, Reliant looks at zoning differently. We can provide insights on what are potentially risky or expensive issues that and what issues can be ignored. Often municipalities intend for their zoning ordinances to be very clear however many have vague clauses that can leave its meaning up to interpretation<br><br>
+                    Reliant Due Diligence only uses top educated reviewers that have degrees in land use or real estate or many years of experience as municipal planners.
                 </div>
             </div>
         </div>
@@ -73,7 +73,7 @@
             </div>
             <div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordion">
                 <div class="card-body">
-                    Reliant uses researchers that carry bachelors and masters degrees from some of the best universities in the world. After the research is completed experienced developers and planners review the results of the research and verify the information to be accurate and complete. This two-stage process insures that the complexities of the zoning code are looked at by multiple people to help avoid errors omissions and inaccuracies
+                    Reliant utilizes researchers that have obtained bachelors and masters degrees from some of the best universities in the world. After the research is completed experienced developers and planners review the results of the research and verify the information to be accurate and complete. This two-stage process insures that the complexities of the zoning code are looked at by multiple people to help avoid errors omissions and inaccuracies
                 </div>
             </div>
         </div>
@@ -87,7 +87,7 @@
             </div>
             <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#accordion">
                 <div class="card-body">
-                    Reliant obtains the most recent zoning ordinance from the municipality when you request your report. Some of the other organizations utilize libraries of ordinances that have been collected from years of being in business and never renewed. Each report is also accompanied by a zoning verification letter (if available) provided by the municipality that further ensures that you are getting the most accurate and up to date information. Each essential zoning and land use report is covered by 2m in errors and omissions insurance to show commitment in our reports and give you piece of mind that the information provided is reliable.
+                    Reliant obtains the most recent zoning ordinance from the municipality when you request your report. Some of the other organizations utilize libraries of ordinances that have been collected from years of being in business and never renewed. Each report is also accompanied by a zoning verification letter (if available) provided by the municipality that further ensures that you are getting the most accurate and up to date information. Each essential zoning and land use report is covered by $2,000,000 of in errors and omissions insurance to show commitment in our reports and give you peace of mind that the information provided is reliable.
                 </div>
             </div>
         </div>
@@ -101,7 +101,7 @@
             </div>
             <div id="collapse7" class="collapse" aria-labelledby="heading7" data-parent="#accordion">
                 <div class="card-body">
-                    Reliant can produce reports from just about every municipality in the US. Some of the smallest municipalities that haven’t digitized their ordinances yet may add a few days to the report turn around however it is extremely rare for that to be the case.
+                    Reliant can produce reports from almost every municipality in the United States. Rarely, a small municipality may not have digitized their ordinances. If this happens it may take a few days longer to complete the report. In that case we ensure the customer is notified of the delay as soon as it is discovered.
                 </div>
             </div>
         </div>
@@ -115,7 +115,7 @@
             </div>
             <div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#accordion">
                 <div class="card-body">
-                    The more information that you can provide the better. At the very minimum would be a Tax ID number or street address and the name of the municipality. The more information that is provided will aide us in turning around the report faster and with greater accuracy.
+                    The more information that you can provide the better. The very minimum needed is a tax parcel ID number or street address and the name of the municipality. Providing the most information available will aide us in turning around the report faster and with greater accuracy.
                 </div>
             </div>
         </div>
@@ -129,7 +129,7 @@
             </div>
             <div id="collapse9" class="collapse" aria-labelledby="heading9" data-parent="#accordion">
                 <div class="card-body">
-                    Reliant Due Diligence can provide a draft in as little as 72 hours. There are many factors that affect the timing of the reports completion. The most significant predictor of the time to complete the report is the responsiveness of the municipality. Once contact has been made with the municipality and the ordinance has been obtained the final report can be completed in 5 - 7 business days.
+                    Reliant Due Diligence can provide a draft in as little as 48 hours. There are many factors that affect the timing of the reports completion. The most significant predictor of the time to complete the report is the responsiveness of the municipality. Once contact has been made with the municipality and the ordinance has been obtained the final report can be completed in 5 - 7 business days.
                 </div>
             </div>
         </div>
--- resources/views/cms/services.blade.php
+++ resources/views/cms/services.blade.php
@@ -9,44 +9,43 @@
                 <p>Already Familiar with our process? --><a class="nav-link zonereport" href="{{route('zoneForm')}}">Order
                         your Essential Zoning Report Now</a></p>
 
-                <p>Reliant Due Diligence engages collegiate educated researchers from the world’s top universities and report reviewers that are current and previous land developers and municipal planners. This 2 step process ensures that multiple people review each report to minimize errors and avoid bias in the analysis. We look at each case objectively and provide clear and concise conclusions.</p>
+                <p>Reliant Due Diligence utilizes researchers from the world's top universities along with experienced land developers and municipal planners to create and review the Essential Zoning Report. This two step process ensures that multiple people review each report to minimize errors and avoid bias in the analysis. We look at each case objectively and provide clear and concise conclusions.</p>
                 <div class="row">
                     <div class="col-md-6">
                         <strong>Step One: Research and Report Creation</strong>
-                        <p>Initial drafts of the Essential Zoning Reports are completed by researchers that are
-                            graduates of some of the world’s top universities. These researchers comb through hundreds
+                        <p>Initial drafts of the Essential Zoning Reports are completed by researchers from some of the world’s top universities. They examine hundreds
                             of pages of municipal documents and extract only the relevant information to the subject
                             property.</p>
                         <strong>Step Two: Information Validation and Report Conclusions</strong>
                         <p>Reliant’s Zoning Consultants review the results of the research to ensure completeness,
                             accuracy and request further information if needed. These consultants come from private
                             development companies or are seasoned municipal planners. The consultants take an objective
-                            look at each case and provide clear and concise conclusions based their years of experience.
+                            look at each case and provide clear and concise conclusions.
                             These consultants will be your point of contact should you have questions regarding your
                             report or the information it contains.</p>
-                        <p>Reports can be ordered online and a draft report is ready in a little as 48 hours and a
-                            completed report in 5-7 business days.</p>
+                        <p>Reports can be ordered online. A draft report is available in a little as 48 hours with a
+                            completed report emailed to the customer in 5-7 business days.</p>
                     </div>
                     <div class="col-md-6">
                         <img class="img-responsive mb-3 hero-banner"
                              src="{!! url('public/images/zoning-report.jpg') !!}">
                     </div>
                 </div>
-                <p>Each Essential Zoning Report is back by $2,000,000 in Errors and Omissions Insurance to provide piece of mind and ensure the reports reliability.</p>
+                <p>Each Essential Zoning Report is backed by $2,000,000 of Errors and Omissions Insurance. This provides peace of mind and ensure the reports reliability.</p>
                 <p>Each Essential Zoning Report Includes:</p>
                 <ul>
-                    <li>An executive summary of the findings in the report</li>
-                    <li>An easy to read concise 10 to 12 page report containing the pertinent information on the subject property</li>
-                    <li>A copy of the zoning map around the area of the subject property</li>
+                    <li>An executive summary with conclusions provided by our Zoning Consultants</li>
+                    <li>An easy to read and concise 10 to 12 page report containing the pertinent information on the subject property</li>
+                    <li>A copy of the zoning map including the area around the subject property</li>
                     <li>A copy of the zoning ordinance relevant to the subject property</li>
                     <li>A copy of a Zoning Verification Letter From the municipality (if provided)</li>
                 </ul>
                 <strong>Pricing:</strong>
                 <p>Reliant Due Diligence offers one product. That is the Essential Zoning Report. We find that this is
-                    the best combination of reports available. This has allowed us to streamline our process, eliminate
-                    the duplicate work or items that are not relevant to your needs, and keep our costs down while still
+                    the best combination of reports available. This has allowed us to streamline our process, and eliminate items that are not relevant to your needs. This keeps
+                    our costs down while still
                     utilizing the most talented researchers and reviewers available.</p>
-                <strong>Each report is $450.00 and includes everything above and you do not pay until you are satisfied
+                <strong>Each report is $450.00. You do not pay until you are satisfied
                     with the results.</strong>
                 <p>If you have a project that includes multiple parcels that share the many of the same characteristics
                     please contact us through our contact page and we can discuss reduced pricing for those types of
